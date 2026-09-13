c176f6b2920f41c56551e7ae78ce84e973029765
Cayenya <zenfloracars@gmail.com>
2026-09-13 05:31:33 -0700

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,2 +0,0 @@
-export const metadata = { title: "BLR 99 Corner Icecream Boutique" }
-export default function RootLayout({children}:{children:any}){return(<html><body style={{margin:0}}>{children}</body></html>)}
